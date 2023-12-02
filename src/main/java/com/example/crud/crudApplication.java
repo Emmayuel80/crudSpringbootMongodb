1b1e96722576b84d79c135989ed2de31ea76ffdb
Silvestre Emmanuel <silvestre.ramirez@wundertec.com>
2023-12-01 22:19:07 -0600

    se omite referencia a una clase inexistente

--- src/main/java/com/example/crud/crudApplication.java
+++ src/main/java/com/example/crud/crudApplication.java
@@ -8,9 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class crudApplication implements CommandLineRunner {
 
-	@Autowired
-	private CustomerRepository repository;
-
 	public static void main(String[] args) {
 		SpringApplication.run(crudApplication.class, args);
 	}
